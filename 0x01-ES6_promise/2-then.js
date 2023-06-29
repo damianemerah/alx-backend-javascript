b792c6f6c5a2ad7406957cf47d49d72a3b849b62
root <damianemerah1@gmail.com>
2023-06-29 07:57:51 -0700

    changes

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -4,5 +4,8 @@ export default function handleResponseFromAPI(promise) {
       console.log('Got a response from the API');
       return { status: 200, body: 'success' };
     })
-    .catch(() => new Error());
+    .catch(() => {
+      console.log('Got a response from the API');
+      return new Error();
+    });
 }
--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,15 +1,9 @@
 import { uploadPhoto, createUser } from './utils';
 
-async function handleProfileSignup() {
-  try {
-    const [photoResponse, userResponse] = await Promise.all([
-      uploadPhoto(),
-      createUser(),
-    ]);
-
-    console.log(photoResponse.body, userResponse.firstName, userResponse.lastName);
-  } catch (err) {
-    console.log('Signup system offline');
-  }
+export default function handleProfileSignup() {
+  Promise.all([uploadPhoto(), createUser()])
+    .then(([photoResponse, userResponse]) => {
+      console.log(photoResponse.body, userResponse.firstName, userResponse.lastName);
+    })
+    .catch(() => console.log('Signup system offline'));
 }
-export default handleProfileSignup;
