26da3690618f070cec0c173a063ea5bc5312010e
root <damianemerah1@gmail.com>
2023-06-29 18:16:06 -0700

    task 100

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,11 +1,14 @@
-import { createUser, uploadPhoto } from './utils';
+import { uploadPhoto, createUser } from './utils';
 
-function handleProfileSignup() {
-  return Promise.all([uploadPhoto(), createUser()])
-    .then((data) => {
-      console.log(`${data[0].body} ${data[1].firstName} ${data[1].lastName}`);
-    })
-    .catch(() => console.log('Signup system offline'));
+export default async function asyncUploadUser() {
+  let photo;
+  let user;
+  try {
+    photo = await uploadPhoto();
+    user = await createUser();
+  } catch (error) {
+    photo = null;
+    user = null;
+  }
+  return { photo, user };
 }
-
-export default handleProfileSignup;
