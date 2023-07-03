05a510ade9c9be4bc971a7f3781987ba2daf5cf5
dammy995 <damianemerah1@gmail.com>
2023-07-03 23:34:02 +0100

    task 0

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,21 @@
+function getListStudents() {
+  return [
+    {
+      firstName: 'Guillaume',
+      id: 1,
+      location: 'San Francisco',
+    },
+    {
+      firstName: 'James',
+      id: 2,
+      location: 'Columbia',
+    },
+    {
+      firstName: 'Serena',
+      id: 5,
+      location: 'San Francisco',
+    },
+  ];
+}
+
+console.log(getListStudents());
