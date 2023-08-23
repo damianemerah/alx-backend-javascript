aeeb735cc37976137a65052a884a8056fd31186f
dammy995 <damianemerah1@gmail.com>
2023-08-23 23:02:40 +0100

    task 4

--- 0x05-Node_JS_basic/dist/4-http.dev.js
+++ 0x05-Node_JS_basic/dist/4-http.dev.js
@@ -1,18 +1,17 @@
-const http = require('http');
+"use strict";
 
-const hostname = '127.0.0.1';
-const port = 1245;
+var http = require('http');
 
-const app = http.createServer((_, res) => {
+var hostname = '127.0.0.1';
+var port = 1245;
+var app = http.createServer(function (_, res) {
   responseText = 'Hello Holberton School!';
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.setHeader('Content-Length', responseText.length);
   res.write(Buffer.from(responseText));
 });
-
-app.listen(port, hostname, () => {
+app.listen(port, hostname, function () {
   console.log('Server running at http://' + hostname + ':' + port);
 });
-
 module.exports = app;
